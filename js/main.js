b3f4e4a1942f5d6cf6c4887cd0c2db2a710a65a9
TamaraLavrina <tamara.lavrina@gmail.com>
2021-05-31 21:27:04 +0300

    fix: привести к числу

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-function getRandomInRange(min, max) {
-  if (min < 0 || max < 0) {
-    throw new Error('Уупс!');
-  }
-  return Math.floor(min + Math.random() * (max + 1 - min));
-}
-getRandomInRange(5, 52);
+
 
 function getRandomFloat(min, max, float = '2') {
   if (min < 0 || max < 0) {
     throw new Error('Уупс!');
   }
-  return Number(Math.random() * (max - min) + min).toFixed(float);
+  return Number((Math.random() * (max - min) + min).toFixed(float));
 }
-getRandomFloat(6,15,3);
+getRandomFloat(5, 45, 3);
