31d4d41f5514083e0192b55c71378b1ea6b8d21f
TamaraLavrina <tamara.lavrina@gmail.com>
2021-05-31 20:46:34 +0300

    fix:привести результат к числу

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-// Результат: целое число из диапазона "от...до"
 function getRandomInRange(min, max) {
   if (min < 0 || max < 0) {
     throw new Error('Уупс!');
   }
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
-getRandomInRange(5, 95);
+getRandomInRange(5, 52);
 
 function getRandomFloat(min, max, float = '2') {
   if (min < 0 || max < 0) {
     throw new Error('Уупс!');
   }
-  return (Math.random() * (max - min) + min).toFixed(float);
+  return Number(Math.random() * (max - min) + min).toFixed(float);
 }
-getRandomFloat(5,555,3);
+getRandomFloat(6,15,3);
