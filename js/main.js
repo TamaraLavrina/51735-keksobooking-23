567e838823dc5226b38578b0670a6870b5c480ec
TamaraLavrina <tamara.lavrina@gmail.com>
2021-06-07 21:52:20 +0300

    массив из объектов

--- js/main.js
+++ js/main.js
@@ -1,34 +1,4 @@
-function getRandomFloat(min, max, float = '2') {
-  if (min < 0 || max < 0) {
-    throw new Error('Уупс!');
-  }
-  return Number((Math.random() * (max - min) + min).toFixed(float));
-}
-getRandomFloat(5, 45, 3);
-
-/*
-let author {
-  avatar: адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём.
-  Например, 01, 02 и т. д. Адреса изображений не повторяются.
-}
-
- let offer{  //объект — содержит информацию об объявлении. Состоит из полей:
-  title, строка — заголовок предложения. Придумайте самостоятельно.
-  address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
-  price, число — стоимость. Случайное целое положительное число.
-  type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-  rooms, число — количество комнат. Случайное целое положительное число.
-  guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-  checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-  checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-  features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-  description, строка — описание помещения. Придумайте самостоятельно.
-  photos, массив строк — массив случайной длины из значений:
-}
-*/
 const SIMILAR_ADV_COUNT = 10;
-// eslint-disable-next-line no-useless-concat
-const avatar = `${'img/avatars/user0' + 'getRandomFloat(0, 8, 0)'}${  ''.png}`;
 const TYPE = [
   'palace',
   'flat',
@@ -54,22 +24,34 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+const DESCRIPTION = [
+  'красивое светлое и мухи не кусают. Ну и конечно, евроремонт',
+  'для славян без кошек и детей - Кекс не рекомендует',
+  'можно всех и с детьми, и с кошкамии, и на девичник тоже',
+];
+function getRandomFloat(min, max, float = '2') {
+  if (min < 0 || max < 0) {
+    throw new Error('Уупс!');
+  }
+  return Number((Math.random() * (max - min) + min).toFixed(float));
+}
+
 const createLocation = () => {
-  const latitude = getRandomFloat(35.65000, 35.70000, 5);
-  const length = getRandomFloat(139.70000, 139.80000, 5);
+  const location = {
+    lat: getRandomFloat(35.65000, 35.70000, 5),
+    lng: getRandomFloat(139.70000, 139.80000, 5),
+  };
   return {
-    lat: latitude,
-    lgn: length,
+    'location.lat, location.lng'
   };
 };
-
 const getRandomArrayElement = (elements) => elements[getRandomFloat(0, elements.length - 1, 0)];
-const getRandomElement = () => getRandomFloat(0, 100500, 0);
+const getRandomElement = () => getRandomFloat(0, 100000, 0);
 const createOffer = () => {
-  const randomPrice = getRandomFloat(1, 100500000, 0);
+  const randomPrice = getRandomFloat(1, 1000000, 0);
   return {
-    title: 'Новое предложeние - лучше старого',
-    address: '{{location.x}}, {{location.y}}',
+    title: 'Предложение №',
+    address: createLocation(),
     price: randomPrice,
     type: getRandomArrayElement(TYPE),
     rooms: getRandomElement(),
@@ -77,23 +59,24 @@ const createOffer = () => {
     checkin: getRandomArrayElement(TIME),
     checkout: getRandomArrayElement(TIME),
     features: getRandomArrayElement(FEATURES),
-    description: 'красивое светлое и мухи не кусают. Ну и конечно, евроремонт',
+    description: getRandomArrayElement(DESCRIPTION),
     photos: getRandomArrayElement(PHOTOS),
   };
 };
 
+
+const createAvatar = () => {
+  const advAvatar = getRandomFloat(0, 8, 0);
+  return {
+    avatar: `img/avatars/user0${advAvatar}.png`,
+  };
+};
+
+
 const createAdvertisement = () => ({
-  author: avatar,
+  author: createAvatar(),
   Offer: createOffer(),
   location: createLocation(),
 });
-
-// eslint-disable-next-line no-console
-console.log(
-  createOffer(),
-);
-
-
 const similaraAvertisement = new Array(SIMILAR_ADV_COUNT).fill(null).map(() => createAdvertisement());
-console.log(similaraAvertisement);
 
